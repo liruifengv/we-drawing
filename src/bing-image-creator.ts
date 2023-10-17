1fa5833584525f87846fdaa94bd17ca9d32e079a
liruifengv <liruifeng1024@gmail.com>
2023-10-17 17:17:17 +0800

    Update Code

--- src/bing-image-creator.ts
+++ src/bing-image-creator.ts
@@ -1,6 +1,5 @@
 
-import crypto from 'node:crypto'
-
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
 const BING_URL: string = process.env.BING_URL || "https://www.bing.com";
 
 // Generate random IP between range 13.104.0.0/14
@@ -13,7 +12,6 @@ const HEADERS: {[key: string]: string} = {
   "referrer": "https://www.bing.com/images/create/",
   "origin": "https://www.bing.com",
   "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
-  "x-forwarded-for": FORWARDED_IP,
 };
 
 
@@ -40,7 +38,7 @@ export class BingImageCreator {
     formData.append("q", encodedPrompt);
     formData.append("qa", "ds");
     console.log("Sending request...")
-    const url = `${BING_URL}/images/create?q=${encodedPrompt}&rt=4&FORM=GENCRE`
+    const url = `${BING_URL}/images/create?q=${encodedPrompt}&rt=3&FORM=GENCRE`
 
     return fetch(url, {
       headers: {
@@ -50,86 +48,87 @@ export class BingImageCreator {
       body: formData,
       method: 'POST',
       mode: 'cors',
-      redirect: "manual",
+      redirect: "manual", // set to manual to prevent redirect
     }).then(async (res) => {
       if (res.ok) {
-        console.log('res', res.status);
-        // check for content waring message
-        const content = await res.text();
-        if (content.toLowerCase().includes("this prompt is being reviewed")) {
-            throw new Error("this prompt is being reviewed");
-        }
-        if (content.toLowerCase().includes("this prompt has been blocked")) {
-            throw new Error("this prompt has been blocked");
-        }
-        if (content.toLowerCase().includes("we're working hard to offer image creator in more languages")) {
-            throw new Error("we're working hard to offer image creator in more languages");
-        }
-        throw new Error("Redirect failed")
+        // 200 is failed
+        throw new Error('Request failed');
       } else {
+        // 302 is success
         const redirect_url = res.headers.get("location").replace("&nfy=1", "");
         const request_id = redirect_url.split("id=")[1];
         console.log('redirect_url', redirect_url);
         console.log('request_id', request_id);
-        await fetch(`${BING_URL}${redirect_url}`, {
-            method: 'POST',
+        try {
+          await fetch(`${BING_URL}${redirect_url}`, {
+            method: 'GET',
             mode: 'cors',
             credentials: 'include',
-        })
+          })
+        } catch (e) {
+          throw new Error('Request failed2', e);
+        }
+
         const getResultUrl = `${BING_URL}/images/create/async/results/${request_id}?q=${encodedPrompt}`
         const start_wait = Date.now();
-        let content = "";
+        let result = "";
         while (true) {
             if (Date.now() - start_wait > 200000) {
                 throw new Error('Timeout');
             }
-            const response = await fetch(getResultUrl, {
-              method: 'POST',
-              mode: 'cors',
-              credentials: 'include',
-            });
-            if (response.status !== 200) {
-              
-                throw new Error('Bad status code');
-            }
-            content = await response.text();
-            console.log('content', content);
-            if (!content || content.includes("errorMessage")) {
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                console.log('Waiting for result...')
-                continue;
-            } else {
+
+            await sleep(1000)
+            result = await this.getResults(getResultUrl);
+            if (result) {
                 break;
             }
         }
-        // Use regex to search for src=""
-        const regex = /src="([^"]*)"/g;
-        const matches = content.match(regex);
-        // # Remove size limit
-        const normal_image_links = matches.map((link) => {
-            return link.split("?w=")[0];
-        });
-        // Remove duplicates
-        const unique_image_links = [...new Set(normal_image_links)];
-        const bad_images = [
-            "https://r.bing.com/rp/in-2zU3AJUdkgFe7ZKv19yPBHVs.png",
-            "https://r.bing.com/rp/TX9QuO3WzcCJz1uaaSwQAz39Kb0.jpg",
-        ];
-        for (const img of unique_image_links) {
-            if (bad_images.includes(img)) {
-                throw new Error("Bad images");
-            }
-        }
-        // No images
-        if (unique_image_links.length === 0) {
-            throw new Error("error_no_images");
-        }
-        return unique_image_links;
+        return this.parseResult(result);
+
       }
     })
   }
 
-  async getResults(requestId: string) {
-    
+  async getResults(getResultUrl: string) {
+    const response = await fetch(getResultUrl, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+    });
+    if (response.status !== 200) {
+        throw new Error('Bad status code');
+    }
+    const content = await response.text();
+    console.log('content', content);
+    if (!content || content.includes("errorMessage")) {
+        return null
+    } else {
+        return content;
+    }
+  }
+  parseResult(result: string) {
+      // Use regex to search for src=""
+      const regex = /src="([^"]*)"/g;
+      const matches = result.match(regex);
+      // # Remove size limit
+      const normal_image_links = matches.map((link) => {
+          return link.split("?w=")[0];
+      });
+      // Remove duplicates
+      const unique_image_links = [...new Set(normal_image_links)];
+      const bad_images = [
+          "https://r.bing.com/rp/in-2zU3AJUdkgFe7ZKv19yPBHVs.png",
+          "https://r.bing.com/rp/TX9QuO3WzcCJz1uaaSwQAz39Kb0.jpg",
+      ];
+      for (const img of unique_image_links) {
+          if (bad_images.includes(img)) {
+              throw new Error("Bad images");
+          }
+      }
+      // No images
+      if (unique_image_links.length === 0) {
+          throw new Error("error_no_images");
+      }
+      return unique_image_links;
   }
 }
