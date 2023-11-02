64e8b10c9b66ea0611a4390d3b8faa0974e33a83
liruifengv <liruifeng1024@gmail.com>
2023-11-02 19:16:15 +0800

    chore: deploy vercel

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
