a515805849a482860980727a638916103f12c521
liruifengv <liruifeng1024@gmail.com>
2023-11-04 14:42:52 +0800

    fix: add 'textless' to prompt

--- src/get-up.ts
+++ src/get-up.ts
@@ -27,7 +27,7 @@ async function getImageBySentence(cookie: string): Promise<Response> {
   const res = await getSentence()
   console.log("getSentence Result: ", res)
 
-  const prompt = res.content
+  const prompt = `${res.content}, textless`
   try {
     const images = await bingImageCreator.createImage(prompt)
     return {
